952d6b83b55391eec4ba14d475e5d362340d87f9
Decclan <61689807+Decclan@users.noreply.github.com>
2026-01-25 17:00:57 +0000

    Nether Fossils Added
    Masonaie varients

--- src/main/java/com/deimoshexxus/netherhexedkingdom/worldgen/ModBiomeModifiers.java
+++ src/main/java/com/deimoshexxus/netherhexedkingdom/worldgen/ModBiomeModifiers.java
@@ -15,15 +15,30 @@ import net.neoforged.neoforge.registries.NeoForgeRegistries;
 public class ModBiomeModifiers {
     public static final ResourceKey<BiomeModifier> ADD_MILITUS_ALLOY_ORE = registerKey("add_militus_alloy_ore");
 
+//    public static final ResourceKey<BiomeModifier> ADD_MASONIAE_NETHER_FOSSILS = registerKey("add_masoniae_nether_fossils");
+
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
         // CF -> PF -> BM
         var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
         var biomes = context.lookup(Registries.BIOME);
 
+        // Ores
         context.register(ADD_MILITUS_ALLOY_ORE, new BiomeModifiers.AddFeaturesBiomeModifier(
                 biomes.getOrThrow(BiomeTags.IS_NETHER),
                 HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.MILITUS_ALLOY_ORE_PLACED_KEY)),
                 GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        // Biome Features
+//        context.register(
+//                ADD_MASONIAE_NETHER_FOSSILS,
+//                new BiomeModifiers.AddFeaturesBiomeModifier(
+//                        biomes.getOrThrow(BiomeTags.IS_NETHER),
+//                        HolderSet.direct(
+//                                placedFeatures.getOrThrow(ModPlacedFeatures.MASONIAE_NETHER_FOSSIL_PLACED)
+//                        ),
+//                        GenerationStep.Decoration.UNDERGROUND_DECORATION
+//                )
+//        );
 
     }
 
--- src/main/java/com/deimoshexxus/netherhexedkingdom/worldgen/ModConfiguredFeatures.java
+++ src/main/java/com/deimoshexxus/netherhexedkingdom/worldgen/ModConfiguredFeatures.java
@@ -13,6 +13,7 @@ import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfigur
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
 import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.feature.configurations.NetherForestVegetationConfig;
 
 import java.util.List;
 
@@ -20,13 +21,51 @@ public class ModConfiguredFeatures {
 
     public static final ResourceKey<ConfiguredFeature<?, ?>> MILITUS_ALLOY_ORE_KEY = registerKey("militus_alloy_ore");
 
+//    public static final ResourceKey<ConfiguredFeature<?, ?>> MASONIAE_NETHER_FOSSIL =
+//            ResourceKey.create(
+//                    Registries.CONFIGURED_FEATURE,
+//                    ResourceLocation.fromNamespaceAndPath(
+//                            NetherHexedKingdom.MODID,
+//                            "masoniae_nether_fossil"
+//                    )
+//            );
+
     public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
+        // Ores
         RuleTest netherrackReplacables = new BlockMatchTest(Blocks.NETHERRACK);
 
         List<OreConfiguration.TargetBlockState> netherMilitusAlloyOres = List.of(
                 OreConfiguration.target(netherrackReplacables, ModBlocks.MILITUS_ALLOY_ORE.get().defaultBlockState()));
 
         register(context, MILITUS_ALLOY_ORE_KEY, Feature.ORE, new OreConfiguration(netherMilitusAlloyOres, 5));
+
+        // Biome Features
+
+//        var pools = context.lookup(Registries.TEMPLATE_POOL);
+//
+//        var masoniaePool = pools.getOrThrow(
+//                ResourceKey.create(
+//                        Registries.TEMPLATE_POOL,
+//                        ResourceLocation.fromNamespaceAndPath(
+//                                NetherHexedKingdom.MODID,
+//                                "nether_fossils/masoniae"
+//                        )
+//                )
+//        );
+
+//        // Vanilla uses the same pool for bones + skulls
+//        NetherFossilConfiguration config =
+//                new NetherFossilConfiguration(
+//                        masoniaePool,
+//                        masoniaePool,
+//                        0.1F // coal replacement chance
+//                );
+//
+//        context.register(
+//                MASONIAE_NETHER_FOSSIL,
+//                new ConfiguredFeature<>(Feature.NETHER_FOSSIL, config)
+//        );
+
     }
 
     public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
